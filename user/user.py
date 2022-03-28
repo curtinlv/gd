f8ba41174d0a843a5996b113bf863541ad225581
curtinlv <curtinlv@126.com>
2022-03-28 23:07:10 +0800

    更改监控变量为自定义方式配置，详见 conf/jk.json 更新方式： 1、以发送机器人命令方式： # 下载自定义监控配置文件 jk.json /cmd cd /ql/config && wget https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/main/conf/jk.json # 更新user.py /cmd cd /ql/jbot/user && rm -f user.py  && wget https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/main/user/user.py # 更新 utils.py /cmd cd /ql/jbot/diy && rm -f utils.py  && wget https://git.metauniverse-cn.com/https://raw.githubusercontent.com/curtinlv/gd/main/diy/utils.py # 重启机器人生效 /reboot
    2.ssh进入容器方式：
    # 下载自定义监控配置文件 jk.json
    cd /ql/repo/gd && git pull  && cp -a /ql/repo/gd/conf/jk.json /ql/config
    # 更新user.py
    rm -f /ql/jbot/user/user.py && cp -a /ql/repo/gd/user/user.py /ql/jbot/user/user.py
    # 更新 utils.py
    rm -f /ql/jbot/diy/utils.py && cp -a /ql/repo/gd/diy/utils.py /ql/jbot/diy/utils.py
    # 重启机器人生效
    pm2 restart jbot

--- user/user.py
+++ user/user.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-
 from asyncio import sleep
 import asyncio
 import os
@@ -10,13 +8,30 @@ import sys
 from telethon import events
 from .login import user
 from .. import chat_id, jdbot, logger, TOKEN
-from ..bot.utils import cmd, V4 
-from ..diy.utils import rwcon, myzdjr_chatIds, my_chat_id
+from ..bot.utils import cmd, V4
+from ..diy.utils import rwcon, myzdjr_chatIds, my_chat_id, jk
 
 bot_id = int(TOKEN.split(":")[0])
 myzdjr_chatIds.append(bot_id)
 client = user
 
+## 新增可通过配置文件自定义监控
+jk_list = jk["jk"]
+cmdName = jk["cmdName"]
+nameList, envNameList, scriptPathList = [], [], []
+for i in jk_list:
+    if i["isOpen"]:
+        nameList.append(i["name"])
+        envNameList.append(i["envName"])
+        scriptPathList.append(i["scriptPath"])
+patternStr = ''
+envNum = len(envNameList)
+for i in range(envNum):
+    if i == envNum-1:
+        patternStr += envNameList[i] + "|jd_redrain_url|jd_redrain_half_url|zjdbody"
+    else:
+        patternStr += envNameList[i] + "|"
+########
 
 @client.on(events.NewMessage(chats=bot_id, from_users=chat_id, pattern=r"^user(\?|\？)$"))
 async def user(event):
@@ -33,39 +48,26 @@ async def user(event):
         logger.error(f"错误--->{str(e)}")
 
 
-@client.on(events.NewMessage(chats=myzdjr_chatIds, pattern=r'export\s(zjdbody|computer_activityId|comm_activityIDList|jd_mhurlList|jd_nzmhurl|wish_appIdArrList|jd_redrain_half_url|jd_redrain_url|M_WX_COLLECT_CARD_URL|M_WX_ADD_CART_URL|M_WX_LUCK_DRAW_URL|WXGAME_ACT_ID|jd_cjhy_activityId|jd_zdjr_activityId).*=(".*"|\'.*\')'))
+@client.on(events.NewMessage(chats=myzdjr_chatIds, pattern=r'export\s(%s).*=(".*"|\'.*\')' % patternStr))
 async def activityID(event):
     try:
         text = event.message.text
-        if "computer_activityId" in text:
-            name = "电脑配件"
-        elif "comm_activityIDList" in text:
-            name = "jdjoy_open通用ID任务"
-        elif "jd_mhurlList" in text:
-            name = "盲盒任务抽京豆"
-        elif "jd_nzmhurl" in text:
-            name = "女装盲盒抽京豆"
-        elif "wish_appIdArrList" in text:
-            name = "许愿池抽奖机"
-        elif "jd_redrain_url" in text:
-            name = "整点京豆雨"
-        elif "jd_redrain_half_url" in text:
-            name = "半点京豆雨"
-        elif "M_WX_COLLECT_CARD_URL" in text:
-            name = "集卡任务"
-        elif "M_WX_ADD_CART_URL" in text:
-            name = "WX加购任务"
-        elif "jd_cjhy_activityId" in text:
-            name = "cj组队瓜分"
-        elif "jd_zdjr_activityId" in text:
-            name = "lz组队瓜分"
-        elif "M_WX_LUCK_DRAW_URL" in text:
-            name = "转盘抽奖"
-        elif "WXGAME_ACT_ID" in text:
-            name = "打豆豆"
-        elif "zjdbody" in text:
-            name = "赚喜豆-每天90豆"
-        else:
+        name = None
+        for i in envNameList:
+            if i in text:
+                name = nameList[envNameList.index(i)]
+                scriptPath = scriptPathList[envNameList.index(i)]
+                break
+            elif "zjdbody" in text:
+                name = "赚喜豆-每天90豆"
+                break
+            elif "jd_redrain_url" in text:
+                name = "整点京豆雨"
+                break
+            elif "jd_redrain_half_url" in text:
+                name = "半点京豆雨"
+                break
+        if not name:
             return
         msg = await jdbot.send_message(chat_id, f'【监控】 监测到`{name}` 环境变量！')
         messages = event.message.text.split("\n")
@@ -103,40 +105,30 @@ async def activityID(event):
             await jdbot.edit_message(msg, f"【取消】 `{name}` 环境变量无需改动！")
             return
         try:
-            if "computer_activityId" in event.message.text:
-                await cmd('task /ql/scripts/jd_computer.js now')
-            elif "comm_activityIDList" in event.message.text:
-                await cmd('task /ql/scripts/jd_joyjd_open.js now')
-            elif "jd_mhurlList" in event.message.text:
-                await cmd('task /ql/scripts/jd_mhtask.js now')
-            elif "jd_nzmhurl" in event.message.text:
-                await cmd('task /ql/scripts/jd_nzmh.js now')
-            elif "wish_appIdArrList" in event.message.text:
-                await cmd('task /ql/scripts/jd_wish.js now')
-            elif "M_WX_COLLECT_CARD_URL" in event.message.text:
-                await cmd('task /ql/scripts/m_jd_wx_collectCard.js now')
-            elif "M_WX_ADD_CART_URL" in event.message.text:
-                await cmd('task m_jd_wx_addCart.js now')
-            elif "M_WX_LUCK_DRAW_URL" in event.message.text:
-                await cmd('task m_jd_wx_luckDraw.js now')
-            elif "WXGAME_ACT_ID" in event.message.text:
-                await cmd('task jd_dadoudou.js now')
-            elif "jd_cjhy_activityId" in event.message.text:
-                await cmd('task /ql/scripts/jd_cjzdgf.js now')
-            elif "jd_zdjr_activityId" in event.message.text:
-                await cmd('task /ql/scripts/jd_zdjr.js now')
-            # 赚京豆助力，将获取到的团body发给自己测试频道
-            elif str(event.message.peer_id.channel_id) in str(my_chat_id) and "zjdbody" in event.message.text:
-                await cmd('task /ql/scripts/zxd.js now')
-            elif "jd_redrain_url" in event.message.text:
-                msg = await jdbot.send_message(chat_id, r'`更换整点雨url完毕\n请定时任务0 0 * * * task jd_redrain now')
-                await asyncio.sleep(1)
-                await jdbot.delete_messages(chat_id, msg)
-            elif "jd_redrain_half_url" in event.message.text:
-                msg = await jdbot.send_message(chat_id, r'`更换半点雨url完毕\n请定时任务30 21,22 * * * task jd_redrain_half now')
-                await asyncio.sleep(1)
-                await jdbot.delete_messages(chat_id, msg)
-            else:
+            lable = None
+            for i in envNameList:
+                if i in text:
+                    lable = True
+                    await cmd(f'{cmdName} {scriptPath} now')
+                    break
+                # 赚京豆助力，将获取到的团body发给自己测试频道，仅自己内部助力使用
+                elif str(event.message.peer_id.channel_id) in str(my_chat_id) and "zjdbody" in text:
+                    lable = True
+                    await cmd('task /ql/scripts/zxd.js now')
+                    break
+                elif "jd_redrain_url" in text:
+                    lable = True
+                    msg = await jdbot.send_message(chat_id, r'`更换整点雨url完毕\n请定时任务0 0 * * * task jd_redrain now')
+                    await asyncio.sleep(1)
+                    await jdbot.delete_messages(chat_id, msg)
+                    break
+                elif "jd_redrain_half_url" in text:
+                    lable = True
+                    msg = await jdbot.send_message(chat_id, r'`更换半点雨url完毕\n请定时任务30 21,22 * * * task jd_redrain_half now')
+                    await asyncio.sleep(1)
+                    await jdbot.delete_messages(chat_id, msg)
+                    break
+            if not lable:
                 await jdbot.edit_message(msg, f"看到这行字,是有严重BUG!")
         except ImportError:
             pass
