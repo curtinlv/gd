3d48020e03d7ba9367576c5446dc882042c8d553
curtinlv <curtinlv@126.com>
2022-04-06 06:52:21 +0800

    update

--- update.sh
+++ update.sh
@@ -1,20 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#set -e
 set -e
+
+daili='https://git.metauniverse-cn.com'
 
 gitPull(){
     echo -e "\n开始更新gd机器人\n"
     rm -rf /ql/repo/gd
-    cd /ql/repo/
-    echo "cd /ql/repo/"
-    /usr/bin/git clone https://git.metauniverse-cn.com/https://github.com/curtinlv/gd.git
-    echo "/usr/bin/git clone https://git.metauniverse-cn.com/https://github.com/curtinlv/gd.git"
+    cd /ql/repo/ && git clone ${daili}/https://github.com/curtinlv/gd.git
     rm -rf /ql/repo/dockerbot
     mkdir /ql/repo/dockerbot
     ln -sf /ql/repo/gd /ql/repo/dockerbot/jbot
-    /usr/local/bin/pm2 stop jbot
-    rm -rf /ql/jbot/*
-    cp -a /ql/repo/gd/* /ql/jbot/
-    /usr/local/bin/pm2 start jbot
+    pm2 stop jbot && rm -rf /ql/jbot/* && cp -a /ql/repo/gd/* /ql/jbot/ && pm2 start jbot
 }
 
 # start
@@ -24,3 +21,4 @@ echo -e "\n\t\t\t【更新机器人】\n"
 echo
 gitPull
 echo -e "已完成更新"
+
