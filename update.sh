2a531d8e31ac467eddbf56ff9327d3d1b1598347
curtinlv <curtinlv@126.com>
2022-04-05 21:17:18 +0800

    v1.2.4 * 修复变量去掉一些奇怪符号.... * 修复user？不回复问题

--- update.sh
+++ update.sh
@@ -6,11 +6,11 @@ daili='https://git.metauniverse-cn.com'
 gitPull(){
     echo -e "\n开始更新gd机器人\n"
     rm -rf /ql/repo/gd
-    cd /ql/repo/ && git clone ${daili}/https://github.com/curtinlv/gd.git
+    cd /ql/repo/ && /usr/bin/git clone ${daili}/https://github.com/curtinlv/gd.git
     rm -rf /ql/repo/dockerbot
     mkdir /ql/repo/dockerbot
     ln -sf /ql/repo/gd /ql/repo/dockerbot/jbot
-    pm2 stop jbot && rm -rf /ql/jbot/* && cp -a /ql/repo/gd/* /ql/jbot/ && pm2 start jbot
+    /usr/local/bin/pm2 stop jbot && rm -rf /ql/jbot/* && cp -a /ql/repo/gd/* /ql/jbot/ && /usr/local/bin/pm2 start jbot
 }
 
 # start
