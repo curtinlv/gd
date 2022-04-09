19b171c9d05d3ad00d9eb84fdf9b25a4af482762
curtinlv <curtinlv@126.com>
2022-04-09 16:16:01 +0800

    * 兼容青龙版本2.12.x * 新增清理功能 /clean * 修复一些user功能的问题

--- update.sh
+++ update.sh
@@ -12,10 +12,12 @@ echo -e "当前青龙版本为:${QL_BRANCH}\n"
 
 gitPull(){
     echo -e "\n开始更新gd机器人\n"
+    rm -rf ${QLMainPath}repo/dockerbot/*
     rm -rf ${QLMainPath}/repo/gd
     cd ${QLMainPath}/repo/ && git clone ${daili}/https://github.com/curtinlv/gd.git
-    rm -rf ${QLMainPath}repo/dockerbot
-    mkdir ${QLMainPath}/repo/dockerbot
+    if [ ! -d ${QLMainPath}/repo/dockerbot ];then
+        mkdir ${QLMainPath}/repo/dockerbot
+    fi
     ln -sf ${QLMainPath}/repo/gd ${QLMainPath}/repo/dockerbot/jbot
     ln -sf ${QLMainPath}/repo/gd/conf ${QLMainPath}/repo/dockerbot/config
     pm2 stop jbot && rm -rf ${QLMainPath}/jbot/* && cp -a ${QLMainPath}/repo/gd/* ${QLMainPath}/jbot/ && pm2 start jbot
