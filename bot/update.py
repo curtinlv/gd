5f7dd7f329a3929f5cd5039421d7e38838372cc9
curtinlv <curtinlv@126.com>
2022-07-22 00:58:05 +0800

    修改user

--- bot/update.py
+++ bot/update.py
@@ -1,6 +1,6 @@
-version = 'v1.3.3'
+version = 'v1.3.4'
 botlog = """
-2022.7.21 (v1.3.3)
+2022.7.22 (v1.3.4)
     * 修复/user登录问题。（如无反应请修改botset.json中最后一个Ture改为False，再重启发送/user 开启user登录）
     * 修复 RPCError 400: ENTITY_BOUNDS_INVALID (caused by SendMessageRequest) 报错
     * 更换代理或关闭github下载代理
